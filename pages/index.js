79ee26194cfa480c8c1d5b6ed0849a2809f06a28
Geice Gomes <sogmesmo@gmail.com>
2021-01-25 18:53:26 -0300

    primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+  
+const BackgroundImage = styled.div`
+    background-image: url(${db.bg});
+    flex: 1;
+    background-size: cover;
+    background-position:center; 
+   `;
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+    <QuizContainer>
+      <Widget>        
+          <Widget.Header>
+            <h1>Grey's Anatomy Quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. </p>  
+          </Widget.Content>        
+      </Widget>
+      <Widget>        
+        <Widget.Header>
+        <h1> Quiz da Galera </h1>
+        </Widget.Header>
+        <Widget.Content>
+        <p>
+            Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração
+        </p>        
+        </Widget.Content>
+      </Widget>
+    </QuizContainer>
+    </BackgroundImage>
+
+  ) 
 }
